01dd229d567a38b9a9dc458ae1a87376efd6bfbc
mofayez22 <mohammedfayez691@gmail.com>
2026-01-22 10:12:31 +0200

    log: added logs for debugging video writer

--- app.py
+++ app.py
@@ -265,10 +265,12 @@ def main():
                 with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_vid:
                     temp_vid.write(video_file.read())
                     input_video_path = temp_vid.name
-                    print(f"###################### {input_video_path}###############")
+                    print(f"######################input video path: {input_video_path}###############")
 
                 output_video_path = process_video(model, input_video_path, conf_threshold)
-                print(f"###################### output video path: {output_video_path}################")
+                st.write("Output path:", output_video_path)
+                st.write("File exists:", os.path.exists(output_video_path))
+                st.write("File size (bytes):", os.path.getsize(output_video_path))
 
             st.success("Done!")
             st.video(output_video_path)
